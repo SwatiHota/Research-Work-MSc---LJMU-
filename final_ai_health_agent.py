3e4f340e4b8c5c23652772691cea881ed935f473
Swati Hota <swatihota66@gmail.com>
2025-08-19 12:10:06 +0530

    Update final_ai_health_agent.py

--- final_ai_health_agent.py
+++ final_ai_health_agent.py
@@ -4,14 +4,14 @@ import requests
 from datetime import datetime
 import sqlite3
 import os
-from dotenv import load_dotenv
 
 
-TOGETHER_API_KEY = st.secrets["tgp_v1_9XbcpxVHW9-j7ZfyU8ssmSaW1Rb-BIp_o0kQMq4UP80"]
+
+TOGETHER_API_KEY = st.secrets["TOGETHER_API_KEY"]
 
 # Load environment variables
-#load_dotenv()
-#TOGETHER_API_KEY = "tgp_v1_9XbcpxVHW9-j7ZfyU8ssmSaW1Rb-BIp_o0kQMq4UP80"
+# load_dotenv()
+# TOGETHER_API_KEY = "tgp_v1_9XbcpxVHW9-j7ZfyU8ssmSaW1Rb-BIp_o0kQMq4UP80"
 
 # Setup DB for chat history
 conn = sqlite3.connect("chat_history.db", check_same_thread=False)
@@ -126,4 +126,5 @@ with st.expander("📜 Chat History"):
     rows = c.execute("SELECT timestamp, role, message FROM history ORDER BY timestamp DESC LIMIT 10").fetchall()
     for ts, role, msg in rows:
         st.markdown(f"**{role.capitalize()}** ({ts.split('T')[0]}): {msg}")
+
 
